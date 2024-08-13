30b00479517246ed7d2cde54b34d506579b78d2d
Philipp Wagner <philipp@bytefish.de>
2024-08-13 15:14:23 +0200

    Update ApplicationOptions.cs

--- src/OpenSkyFlightTracker.Api/Options/ApplicationOptions.cs
+++ src/OpenSkyFlightTracker.Api/Options/ApplicationOptions.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Philipp Wagner. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using OpenSkyFlightTracker.Api.Options;
-
-namespace OpenSkyBackend.Options
+namespace OpenSkyFlightTracker.Api.Options
 {
     public class ApplicationOptions
     {
